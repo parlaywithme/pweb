9c3d4d8c7fc34e8739793f8d0a4d7fffe2bd3a92
Loren Sands-Ramshaw <lorensr@gmail.com>
2015-01-08 01:19:23 -0800

    fix sid's breakage, fix emoji, more bet -> parlay

--- main.js
+++ main.js
@@ -3,30 +3,34 @@ alert_dev = function () {
 };
 
 window.onload = function() {
+
     links = document.querySelectorAll('.download-links > a');
     if(links.length === 0)
 	return;
 
     links[0].onclick = alert_dev;
     links[1].onclick = alert_dev;
+
 };
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 
+    emojify.run();
+
     window.drawer = document.querySelector('core-drawer-panel');
 
     if(window.screen.width > 640) {
-	drawer.setAttribute('drawerWidth','100%');
+	drawer && drawer.setAttribute('drawerWidth','100%');
     }
 
     // so it doesn't run at pageload
     setTimeout( function() {
 	window.addEventListener('core-responsive-change', function(e) {
     	    if (e.detail.narrow) {
-		drawer.setAttribute('drawerWidth', '256px');
+		drawer && drawer.setAttribute('drawerWidth', '256px');
 	    }
 	    else {
-		drawer.setAttribute('drawerWidth','100%');
+		drawer && drawer.setAttribute('drawerWidth','100%');
 	    }
 	});
     }, 1000);
@@ -34,4 +38,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-
+emojify.setConfig({"img_dir": "bower_components/emojify.js/images/emoji"});
