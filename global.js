da36878d87369a1458b55884303b68281403ec71
hagata <genki@hagata.info>
2015-07-26 15:26:13 -0700

    inits team section rewrites alternate gif function in component js file

--- global.js
+++ global.js
@@ -64,3 +64,28 @@ $(document).ready(function() {
 });
 
 
+//
+var alternateImage = (function(){
+
+  var _altImage = function(name) {
+    $('#' + name).on('mouseenter', function(e){
+      return $(e.target).attr('src', "/images/" + name + "_alt.gif");
+    }).on('mouseleave', function(e){
+      return $(e.target).attr('src', "/images/" + name + ".jpg");
+    });
+  };
+
+  return {
+    gif: _altImage
+  }
+
+})();
+
+
+
+names = ['joint', 'sidney', 'loren'];
+
+for(name in names){
+  alternateImage.gif(names[name]);
+
+}
