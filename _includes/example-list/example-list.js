7686ce203d5078a5503ab96148f9b5356405b4d1
hagata <genki@hagata.info>
2015-07-26 10:45:24 -0700

    cleans up interaction/ layout for examples-list component cleans up mobile styles

--- _includes/example-list/example-list.js
+++ _includes/example-list/example-list.js
@@ -1,11 +1,22 @@
 var expandLink = document.getElementById('expand-link');
 var examples = document.getElementById('example-list');
+var events = ['click', 'touchend'];
 
-expandLink.addEventListener('click', function(){
-  examples.classList.toggle('expanded');
-  if (examples.classList == 'expanded') {;
-    expandLink.innerHTML = "Collapse examples";
-  }else{
-    expandLink.innerHTML = "Show more examples";
-  }
-}, false);
+for(event of events){
+
+  expandLink.addEventListener(event, function(){
+    examples.classList.toggle('expanded');
+    if (examples.classList == 'expanded') {;
+      expandLink.innerHTML = "Collapse examples";
+    }else{
+      expandLink.innerHTML = "Show more examples";
+    }
+console.log('exfired');
+
+  }, false);
+
+}
+
+// window.addEventListener('touchend', function(){
+//   console.log('touched')
+// },false)
