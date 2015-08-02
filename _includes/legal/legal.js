d1f1ac32e593e0c16591045562f672b5e6310724
hagata <genki@hagata.info>
2015-08-02 09:45:14 -0700

    Updates Legal Submenu, legal page template, cover styles,

--- _includes/legal/legal.js
+++ _includes/legal/legal.js
@@ -1,47 +1,34 @@
-$(document).ready(function () {
-  $('.accordion-tabs-minimal').each(function(index) {
-    $(this).children('li').first().children('a').addClass('is-active');
-      $($('.tab-content')[0]).addClass('is-open');
-
-  });
-
-  $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event, index) {
-
-  console.log('clocked', event, ' index ', index);
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs-minimal');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
-
-      $(this).next().toggleClass('is-open').toggle();
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-    } else {
-      event.preventDefault();
-    }
-  });
-});
-
-
-      // $($('.tab-content')[index]).addClass('is-classy');
 
 var events = ['click', 'touchend'];
+var $tabContent = $('.tab-item');
+var locked = false;
 
 $(document).ready(function(){
-//for every legal menu open the first item.
-  $('.legal-menu').children('li').first().addClass('active');
 
-for(event of events){
+  var $legalMenu = $('.legal-menu');
+  var $leftOffset = $legalMenu.offset().left
+  
+  $(window).on('scroll', function(e){
+    var currentScroll = this.scrollY;
 
-  $('.legal-menu').children('li').on(event, function(){
-console.log(event);
-  //remove active
 
-   //assign active for clicked
 
-   //make similar index p also active
+    if(currentScroll > 88 && !locked){
+
+      locked = true;
+      var $shim = $($legalMenu).clone()
+        .attr('id','Shim')
+        .prependTo($($legalMenu).parent()
+      );
+
+      $($legalMenu)
+        .css('left', $leftOffset)
+        .addClass('sticky');      
+    }else if(currentScroll < 88){
+      $($legalMenu).removeClass('sticky')
+      $('#Shim').remove();
+      locked = false;
+    }
   })
 }  
-//for every li, when it's clicked,
-//pass the index -> the same as corresponding content div index
-});
+
